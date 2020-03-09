dc04f9e0c26406cda1588b7846cd277a42e25a9c
Coleman Haley <coleman.c.haley@gmail.com>
2020-03-09 17:13:21 -0400

    Add topic modeling, minor fixes.

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import sys
+
 sys.path.append("..")
 import generating
 
@@ -8,16 +9,18 @@ app = Flask(__name__)
 
 CORS(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return jsonify(["dependency_parsing.txt","language_modelling.txt"])
+    return jsonify(["dependency_parsing.txt", "language_modelling.txt"])
 
 
-@app.route('/resource/<id>', methods=['GET'])
+@app.route("/resource/<id>", methods=["GET"])
 def get_resource(id):
-    with open('../topics/' + id) as f:
-        return jsonify('\n'.join(f.readlines()))
+    with open("../topics/" + id) as f:
+        return jsonify("\n".join(f.readlines()))
 
-@app.route('/generate', methods=['GET'])
+
+@app.route("/generate", methods=["GET"])
 def get_prediction():
-	return jasonify(generating.get_predicted_topics())
+    return jsonify(generating.get_predicted_topics())
--- backend/generating.py
+++ backend/generating.py
@@ -3,18 +3,20 @@ from transformers import GPT2Tokenizer, GPT2Model, BertForMaskedLM, GPT2LMHeadMo
 import numpy as np
 from torch.nn.functional import softmax
 import sys
-gpu = sys.argv[1]
-device = torch.device("cuda:" + str(gpu))
 
-tokenizer = GPT2Tokenizer.from_pretrained('distilgpt2')
+# gpu = sys.argv[1]
+# device = torch.device("cuda:" + str(gpu))
+device = torch.device("cpu")
 
-model = GPT2LMHeadModel.from_pretrained('distilgpt2').to(device)
+tokenizer = GPT2Tokenizer.from_pretrained("distilgpt2")
+
+model = GPT2LMHeadModel.from_pretrained("distilgpt2").to(device)
 model.eval()
 
 
 def get_predictions(topic, problem):
 
-    # topic : string type, 
+    # topic : string type,
     # problem : string type
 
     score = 0
@@ -35,10 +37,12 @@ def get_predictions(topic, problem):
             outputs = model(topic_tensor)
             predictions = outputs[0]
 
-        temp = predictions[0,-1,:]
+        temp = predictions[0, -1, :]
         result = softmax(temp)
         score -= torch.log(result[each_word])
-        topic_tensor = torch.cat((topic_tensor, torch.LongTensor([each_word]).to(device).unsqueeze(0)), 1)[:, 1:]
+        topic_tensor = torch.cat(
+            (topic_tensor, torch.LongTensor([each_word]).to(device).unsqueeze(0)), 1
+        )[:, 1:]
 
     return score
     # print('are')
@@ -46,61 +50,63 @@ def get_predictions(topic, problem):
     # print('is')
     # print(result[iss])
     # print(result[are] > result[iss])
-    
-topics = []
-problems = []
-with open("topics/dependency_parsing.txt", "r") as f:
-    topics.append(f.read()[0:1024])
-with open("topics/language_modelling.txt", "r") as f:
-    topics.append(f.read()[0:1024])
-with open("topics/machine_translation.txt", "r") as f:
-    topics.append(f.read()[0:1024])
-with open("topics/neural_nets.txt", "r") as f:
-    topics.append(f.read()[0:1024])
-with open("topics/vector_semantics.txt", "r") as f:
-    topics.append(f.read()[0:1024])
-with open("questions/q1_mt.txt", "r") as f:
-    problems.append(f.read())
-# predictions: list of list : each element is a list of scores corresponding to the each topic
-predictions = []
-result = []
-
-problem_segments = []
-for each in problems:
-    if len(each) > 1024:
-        segs = []
-        num_segs = len(each) // 1024
-        for i in range(num_segs):
-            segs.append(each[i * len(each): (i+1) * len(each)])
-        segs.append(each[(i+1)*len(each):])
-        problem_segments.append(segs)
 
 
-# loop over the lists of topics and problems 
-for each_problem in problem_segments:
-    if isinstance(each_problem, str):
-        scores = []
-        for each_topic in topics:
-            print(topics.index(each_topic))
-            scores.append(get_predictions(each_topic, each_problem))
-        predictions.append(scores)
-    else:
-        scores = []
-        for each_topic in topics:
-            print(topics.index(each_topic))
-            grade = 0.0
-            for each_seg in each_problem:
-                print(each_seg, len(each_seg))
-                grade += get_predictions(each_topic, each_seg)
-            scores.append(grade)
-        predictions.append(scores)
+def get_predicted_topics():
+    topics = []
+    problems = []
+    with open("../topics/dependency_parsing.txt", "r") as f:
+        topics.append(f.read()[0:1024])
+    with open("../topics/language_modelling.txt", "r") as f:
+        topics.append(f.read()[0:1024])
+    with open("../topics/machine_translation.txt", "r") as f:
+        topics.append(f.read()[0:1024])
+    with open("../topics/neural_nets.txt", "r") as f:
+        topics.append(f.read()[0:1024])
+    with open("../topics/vector_semantics.txt", "r") as f:
+        topics.append(f.read()[0:1024])
+    with open("../questions/q1_mt.txt", "r") as f:
+        problems.append(f.read())
+    # predictions: list of list : each element is a list of scores corresponding to the each topic
+    predictions = []
+    result = []
+
+    problem_segments = []
+    for each in problems:
+        if len(each) > 1024:
+            segs = []
+            num_segs = len(each) // 1024
+            for i in range(num_segs):
+                segs.append(each[i * len(each) : (i + 1) * len(each)])
+            segs.append(each[(i + 1) * len(each) :])
+            problem_segments.append(segs)
+
+    # loop over the lists of topics and problems
+    for each_problem in problem_segments:
+        if isinstance(each_problem, str):
+            scores = []
+            for each_topic in topics:
+                print(topics.index(each_topic))
+                scores.append(get_predictions(each_topic, each_problem))
+            predictions.append(scores)
+        else:
+            scores = []
+            for each_topic in topics:
+                print(topics.index(each_topic))
+                grade = 0.0
+                for each_seg in each_problem:
+                    print(each_seg, len(each_seg))
+                    grade += get_predictions(each_topic, each_seg)
+                scores.append(grade)
+            predictions.append(scores)
+
+    # Take an argmax to find the best topic, or we can find multiple topics further :: TODO
+    for each in predictions:
+        result.append(torch.argmax(torch.Tensor(each)))
+
+    result = [topics[i] for i in result]
+    print(result)
+    return result
 
 
-# Take an argmax to find the best topic, or we can find multiple topics further :: TODO
-for each in predictions:
-    result.append(torch.argmax(torch.Tensor(each)))
-
-result = [topics[i] for i in result]
-print(topics)
-print(predictions)
-print(result)
+get_predicted_topics()
